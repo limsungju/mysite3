b1af046b303a8495a638a200dc70605932795c21
limsungju <fullmoon4860@gmail.com>
2019-09-27 09:21:41 +0900

    SLF4J추가

--- src/main/java/kr/co/itcen/mysite/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/co/itcen/mysite/exception/GlobalExceptionHandler.java
@@ -8,11 +8,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+	private static final Log LOG = LogFactory.getLog( GlobalExceptionHandler.class );
 	
 	// Controller, Dao, Service에서는 기술침투 x, 시스템 외적인 부분에선 o
 	// Exception이 나면 이곳에서 받는다.
